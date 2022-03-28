88d12ce904f66a59ade5927e0ba7f311f6e3ae55
NikitaKul209 <Nikita2099n@mail.ru>
2022-03-28 22:03:37 +0300

    Commit_3

--- src/open_base/script/publisher_node.py
+++ src/open_base/script/publisher_node.py
@@ -15,43 +15,47 @@ from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import SetModelState
 from gazebo_msgs.srv import *
 
-global length_1
-global length_2
-global length_3
-global length_4
-global length_5
-global length_6
-length_1 = 0.4
+length_1 = 1
+length_2 = 1
+length_3 = 1
+length_4 = 1
+length_5 = 1
+length_6 = 1
+
+
 rospy.init_node('Robot_control', anonymous=True)
 
 def  callback_1(msg):
+    global length_1
     length_1 = msg.range
-    rospy.loginfo("Sharp_1 = {} " .format(msg.range))
-    return length_1
+    # rospy.loginfo("Sharp_1 = {} " .format(msg.range))
+
 def sharp_1():
     rospy.Subscriber('/open_base/one_sonar', Range, callback_1)
 
 
-
 def callback_2(msg):
+    global length_2
     length_2 = msg.range
-    rospy.loginfo("Sharp_2 = {} " .format(msg.range))
+    # rospy.loginfo("Sharp_2 = {} " .format(msg.range))
 def sharp_2():
     rospy.Subscriber('/open_base/two_sonar', Range, callback_2)
 
 
 
 def callback_3(msg):
+    global length_3
     length_3 = msg.range
-    rospy.loginfo("Sharp_3 = {} " .format(msg.range))
+    # rospy.loginfo("Sharp_3 = {} " .format(msg.range))
 def sharp_3():
     rospy.Subscriber('/open_base/three_sonar', Range, callback_3)
 
 
 
 def callback_4(msg):
+    global length_4
     length_4 = msg.range
-    rospy.loginfo("Sharp_4 = {} " .format(msg.range))
+    # rospy.loginfo("Sharp_4 = {} " .format(msg.range))
 def sharp_4():
     rospy.Subscriber('/open_base/for_sonar', Range, callback_4)
 
@@ -59,16 +63,18 @@ def sharp_4():
 
 
 def callback_5(msg):
+    global length_5
     length_5 = msg.range
-    rospy.loginfo("Sharp_5 = {} " .format(msg.range))
+    # rospy.loginfo("Sharp_5 = {} " .format(msg.range))
 def sharp_5():
     rospy.Subscriber('/open_base/five_sonar', Range, callback_5)
 
 
 
 def callback_6(msg):
+    global length_6
     length_6 = msg.range
-    rospy.loginfo("Sharp_6 = {} " .format(msg.range))
+    # rospy.loginfo("Sharp_6 = {} " .format(msg.range))
 def sharp_6():
     rospy.Subscriber('/open_base/six_sonar', Range, callback_6)
 
@@ -117,13 +123,20 @@ def set_pose():
 
 set_pose()
 while True:
-
-    move_platform(0.0, 0.5, -0.5)
-    time.sleep(5)
+    move_platform(0.0, 0.3, -0.3)
     sharp_1()
-    if length_1 < 0.6:
-        move_platform(0.0, 0.0, 0.0)
-        time.sleep(5)
+    sharp_2()
+    sharp_3()
+    sharp_4()
+    sharp_5()
+    sharp_6()
+    rospy.loginfo('s1=: {} s2=: {} s3=: {} s4=: {} s5=: {} s6=: {}'.format(length_1,length_2,length_3,length_4,length_5,length_6))
+    if length_1 < 0.5 or length_2 < 0.5 or length_3 < 0.5 or length_4 < 0.5 or length_5 < 0.5 or length_6 < 0.5:
+        move_platform(0.3, 0.3, 0.3)
+        time.sleep(0.01)
+
+
+
 
     # sharp_1()
     # sharp_2()
